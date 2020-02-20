2037fb8d262a0d5429dc646af5a81dac8d4e1381
Ashis Pati <ashis.pati@gatech.edu>
2020-02-20 11:16:43 -0500

    change parsers to work with new MIG-FbaData repository

--- src/dataLoaders/DataUtils.py
+++ src/dataLoaders/DataUtils.py
@@ -1,11 +1,7 @@
-import sys
 import os
-import json
-import dill
-from collections import defaultdict
+import sys
 import numpy as np
 import pandas as pd
-from pandas import ExcelFile
 
 # define bad students ids 
 # for which recording is bad or segment annotation doesn't exist
@@ -30,7 +26,10 @@ class DataUtils(object):
         """
         self.path_to_annotations = path_to_annotations
         self.path_to_audio = path_to_audio
-        self.band = band
+        if band == 'middle':
+            self.band = band + 'school'
+        else:
+            self.band = band + 'band'
         self.instrument = instrument
         self.bad_ids = bad_ids[band]
 
@@ -40,16 +39,9 @@ class DataUtils(object):
         Arg:
                 year:	string, which year
         """
-        if self.band == 'middle':
-            file_name = 'Middle School'
-        elif self.band == 'concert':
-            file_name = 'Concert Band Scores'
-        elif self.band == 'symphonic':
-            file_name = 'Symphonic Band Scores'
-        else:
-            raise ValueError("Invalid value for 'band'")
-        xls_path = 'FBA' + year + '/'
-        xls_file_path = self.path_to_annotations + xls_path + file_name + '.xlsx'
+        folder_name = 'FBA' + year
+        file_name = 'excel' + self.band + '.xlsx'
+        xls_file_path = os.path.join(self.path_to_annotations, folder_name, self.band, file_name)
         return xls_file_path
 
     def get_audio_folder_path(self, year):
@@ -58,52 +50,22 @@ class DataUtils(object):
         Arg:
                 year:   string, which year
         """
-        if self.band == 'middle':
-            folder_name_band = 'middleschool'
-        elif self.band == 'concert':
-            folder_name_band = 'concertband'
-        elif self.band == 'symphonic':
-            folder_name_band = 'symphonicband'
-        else:
-            raise ValueError("Invalid value for 'band'")
-        
+        folder_name_year = year + '-' + str(int(year)+1)
+        band_folder = self.band
         if year == '2013':
-            folder_name_year = '2013-2014'
-        elif year == '2014':
-            folder_name_year = '2014-2015'
-        elif year == '2015':
-            folder_name_year = '2015-2016'
-        else:
-            raise ValueError("Invalid value for 'year'")
-        
-        audio_folder = self.path_to_audio + folder_name_year + '/' + folder_name_band
-        if year == '2013':
-            audio_folder += 'scores/'
-        else:
-            audio_folder += '/'
-        return audio_folder
+            band_folder += 'scores'
+        path_to_audio_folder = os.path.join(self.path_to_audio, folder_name_year, band_folder)
+        return path_to_audio_folder
 
     def get_anno_folder_path(self, year):
         """
-        Returns the full path to the root folder containing all the FBA segment files and
-		assessments
+        Returns the full path to the root folder containing all the FBA segment files and assessments
         Arg:
                 year:	string, which year
         """
-        if self.band == 'middle':
-            folder_name = 'middleschool'
-        elif self.band == 'concert':
-            folder_name = 'concertband'
-        elif self.band == 'symphonic':
-            folder_name = 'symphonicband'
-        else:
-            raise ValueError("Invalid value for 'band'")
-        annotations_folder = self.path_to_annotations + 'FBA' + year + '/' + folder_name
-        if year == '2013':
-            annotations_folder += 'scores/'
-        else:
-            annotations_folder += '/'
-        return annotations_folder
+        folder_name = 'FBA' + year
+        path_to_anno_folder = os.path.join(self.path_to_annotations, folder_name, self.band, 'assessments')
+        return path_to_anno_folder
     
     def scan_student_ids(self, year):
         """
@@ -136,25 +98,20 @@ class DataUtils(object):
         """
         Returns the segment info for the provide inputs as a list of tuples (start_time, end_time)
         Args:
-                year:			string, which year
-                segment:		string, which segment
-                student_ids:	list, containing the student ids., if empty we compute it within this
-								 function
+            year:			string, which year
+            segment:		string, which segment
+            student_ids:	list, containing the student ids., if empty we compute it within this function
         """
         annotations_folder = self.get_anno_folder_path(year)
         segment_data = []
         if student_ids == []:
             student_ids = self.scan_student_ids(year)
         for student_id in student_ids:
-            segment_file_path = annotations_folder + \
-                str(student_id) + '/' + str(student_id) + '_segment.txt'
+            segment_file_path = os.path.join(annotations_folder, str(student_id), str(student_id) + '_segment.txt')
             file_info = [line.rstrip('\n')
                          for line in open(segment_file_path, 'r')]
             segment_info = file_info[segment]
-            if sys.version_info[0] < 3:
-                to_floats = map(float, segment_info.split('\t'))
-            else:
-                to_floats = list(map(float, segment_info.split('\t')))
+            to_floats = list(map(float, segment_info.split('\t')))
             # convert to tuple and append
             segment_data.append((to_floats[0], to_floats[0] + to_floats[1]))
         return segment_data
@@ -164,10 +121,9 @@ class DataUtils(object):
         Returns the pitch contours for the provide inputs as a list of np arrays
                 assumes pyin pitch contours have already been computed and stored as text files
         Args:
-                year:			string, which year
-                segment:		string, which segment
-                student_ids:	list, containing the student ids., if empty we compute it within this
-								 function
+            year:			string, which year
+            segment_info:		string, which segment
+            student_ids:	list, containing the student ids., if empty we compute it within this function
         """
         data_folder = self.get_anno_folder_path(year)
         if student_ids == []:
@@ -175,18 +131,13 @@ class DataUtils(object):
         pitch_contour_data = []
         idx = 0
         for student_id in student_ids:
-            pyin_file_path = data_folder + \
-                str(student_id) + '/' + str(student_id) + \
-                '_pyin_pitchtrack.txt'
+            pyin_file_path = os.path.join(data_folder, str(student_id), str(student_id) + '_pyin_pitchtrack.txt')
             lines = [line.rstrip('\n') for line in open(pyin_file_path, 'r')]
             pitch_contour = []
             start_time, end_time = segment_info[idx]
             idx = idx + 1
             for x in lines:
-                if sys.version_info[0] < 3:
-                    to_floats = map(float, x.split(','))
-                else:
-                    to_floats = list(map(float, x.split(',')))
+                to_floats = list(map(float, x.split(',')))
                 timestamp = to_floats[0]
 
                 if timestamp < start_time:
@@ -206,9 +157,8 @@ class DataUtils(object):
         """
         Returns the audio paths for the provide inputs as a list of strings
         Args:
-                year:           string, which year
-                student_ids:    list, containing the student ids., if empty we compute it within this
-                                 function
+            year:           string, which year
+            student_ids:    list, containing the student ids., if empty we compute it within this function
         """
         data_folder = self.get_audio_folder_path(year)
         if student_ids == []:
@@ -223,13 +173,11 @@ class DataUtils(object):
 
     def get_perf_rating_segment(self, year, segment, student_ids=[]):
         """
-        Returns the performane ratings given by human judges for the input segment as a list of
-		tuples
+        Returns the performane ratings given by human judges for the input segment as a list of tuples
         Args:
-                year:			string, which year
-                segment:		string, which segment
-                student_ids:	list, containing the student ids., if empty we compute it within
-								 this function
+            year:			string, which year
+            segment:		string, which segment
+            student_ids:	list, containing the student ids., if empty we compute it within this function
         """
         annotations_folder = self.get_anno_folder_path(year)
         perf_ratings = []
@@ -237,15 +185,11 @@ class DataUtils(object):
             student_ids = self.scan_student_ids(year)
 
         for student_id in student_ids:
-            ratings_file_path = annotations_folder + \
-                str(student_id) + '/' + str(student_id) + '_assessments.txt'
+            ratings_file_path = os.path.join(annotations_folder, str(student_id), str(student_id) + '_assessments.txt')
             file_info = [line.rstrip('\n')
                          for line in open(ratings_file_path, 'r')]
             segment_ratings = file_info[segment]
-            if sys.version_info[0] < 3:
-                to_floats = map(float, segment_ratings.split('\t'))
-            else:
-                to_floats = list(map(float, segment_ratings.split('\t')))
+            to_floats = list(map(float, segment_ratings.split('\t')))
             # convert to tuple and append
             perf_ratings.append(
                 (to_floats[2], to_floats[3], to_floats[4], to_floats[5]))
@@ -256,8 +200,9 @@ class DataUtils(object):
         """
         Creates the data representation for a particular year
         Args:
-                year:           string, which year
-                segment:		string, which segment
+            year:       string, which year
+            segment:    string, which segment
+            audio:      bool
         """
         if audio:
             import librosa
--- src/dataLoaders/data_process.py
+++ src/dataLoaders/data_process.py
@@ -4,22 +4,18 @@ from collections import defaultdict
 import dill
 import numpy as np
 import scipy.io
-from DataUtils import DataUtils
+from src.dataLoaders.DataUtils import DataUtils
 
 
 # Initialize input params, specify the band, intrument, segment information
-BAND = 'symphonic'
+BAND = 'middle'
 SEGMENT = 2
 YEAR = ['2013', '2014', '2015']
 
 # define paths to FBA dataset and FBA annotations
 # NEED TO EDIT THE PATH HERE IF USING ON A DIFFERENT COMPUTER
-if sys.version_info[0] < 3:
-    PATH_FBA_ANNO = '/Data/FBA2013/'
-    PATH_FBA_AUDIO = '/Data/FBA2013data/'
-else:
-    PATH_FBA_ANNO = '/home/apati/FBA2013/'
-    PATH_FBA_AUDIO = '/home/apati/FBA2013/'
+PATH_FBA_ANNO = os.path.realpath('../../../MIG-FbaData')
+PATH_FBA_AUDIO = os.path.realpath('../../../FBA2013Data')
 
 # create data holder
 perf_assessment_data = []
@@ -43,11 +39,5 @@ for year in YEAR:
 print(len(perf_assessment_data))
 
 file_name = BAND + '_' + str(SEGMENT) + '_data'
-if sys.version_info[0] < 3:
-    with open('../dat/' + file_name + '.dill', 'wb') as f:
-        dill.dump(perf_assessment_data, f)
-    scipy.io.savemat('../dat/' + file_name + '.mat', mdict = {'perf_data': perf_assessment_data})
-else:
-    with open('../dat/' + file_name + '_3.dill', 'wb') as f:
-        dill.dump(perf_assessment_data, f)
- 
+with open('../dat/' + file_name + '.dill', 'wb') as f:
+    dill.dump(perf_assessment_data, f)
