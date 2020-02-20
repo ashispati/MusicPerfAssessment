f46be7aff7b1b305a4e281d1d4f9fb23f3497c4b
Ashis Pati <ashis.pati@gatech.edu>
2020-02-20 14:32:27 -0500

    minor changes to check_dill method

--- src/dataLoaders/check_dill.py
+++ src/dataLoaders/check_dill.py
@@ -1,31 +1,34 @@
+import os
 import dill
 from tqdm import tqdm
 
-BAND ='middle'
+BAND = ['middle', 'symphonic']
 
-path_to_oldData = '/dat/'
+data_folder = os.path.realpath('../dat')
+old_data_folder = 'old_data'
 
 print("Loading dill files...")
-oldFile = open(path_to_oldData + BAND + '_2_data_3.dill', 'rb')
-newFile = open('dat/' + BAND + '_2_data.dill', 'rb')
-dat = dill.load(oldFile)
-newDat = dill.load(newFile)
-print("Data loaded.")
+for band in BAND:
+	oldFile = open(os.path.join(data_folder, old_data_folder, band + '_2_data_3.dill'), 'rb')
+	newFile = open(os.path.join(data_folder, band + '_2_data.dill'), 'rb')
+	dat = dill.load(oldFile)
+	newDat = dill.load(newFile)
+	print("Data loaded.")
 
-if len(dat) != len(newDat):
-	raise ValueError("The old list has length " + str(len(dat)) + " , but the new list has length " + str(len(newDat)))
+	if len(dat) != len(newDat):
+		raise ValueError("The old list has length " + str(len(dat)) + " , but the new list has length " + str(len(newDat)))
 
-for i in tqdm(range(len(dat))):
-	for key, value in dat[i].items():
-		if key == 'band' or 'audio':
-			continue
-		elif key not in newDat[i].keys():
-			print(dat[i].keys())
-			print(newDat[i].keys())
-			raise ValueError("Keys are different at list index " + str(i))
-		elif value not in newDat[i].values():
-			print(dat[i].values())
-			print(newDat[i].values())
-			raise ValueError("Found different values at list index " + str(i))
+	for i in tqdm(range(len(dat))):
+		for key, value in dat[i].items():
+			if key == 'band' or 'audio':
+				continue
+			elif key not in newDat[i].keys():
+				print(dat[i].keys())
+				print(newDat[i].keys())
+				raise ValueError("Keys are different at list index " + str(i))
+			elif value not in newDat[i].values():
+				print(dat[i].values())
+				print(newDat[i].values())
+				raise ValueError("Found different values at list index " + str(i))
 
 print("The dill files are the same!")
--- src/dataLoaders/data_process.py
+++ src/dataLoaders/data_process.py
@@ -9,7 +9,7 @@ from src.dataLoaders.DataUtils import DataUtils
 
 
 # Initialize input params, specify the band, instrument, segment information
-BAND = 'middle'
+BAND = 'symphonic'
 SEGMENT = 2
 YEAR = ['2013', '2014', '2015']
 
